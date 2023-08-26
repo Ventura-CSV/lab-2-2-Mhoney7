5236dfd2c6cb804ce45151f20149e8aa8ea2ba27
Melissa <witchy.kat013@gmail.com>
2023-08-25 22:46:20 -0700

    main.py lab 2-2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    workhours = int(input('Enter your work hours: '))
+    workhours = int(input('50'))
     reg_hours = 40
     reg_rate = 18.25
     ov_rate = 27.78
@@ -12,8 +12,21 @@ def main():
     # regular_wage = reg_hours * reg_rate
     # total_wage = regular_wage + overtime_wage
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
+workhours = 50
+reg_hours = 40
+reg_rate = 18.2
+ov_rate = 27.78
+
+overtime_hours = (workhours - reg_hours)
+overtime_wage = (overtime_hours * ov_rate)
+regular_wage = (reg_hours * reg_rate)
+
+total_wage = (regular_wage + overtime_wage)
+
+
+def main():
+    print(f"Regular Charge: {regular_wage:.2f}")
+    print(f"Overtime Charge: {overtime_wage:.2f}")
     print(f"Total wage : {total_wage:.2f}")
 
    ##################################################
